68019ad1878e78488a1b3b9ecfc38e41eaa08a32
Paddy <paddyfedoo@hotmail.com>
2024-04-10 16:12:53 +0100

    Second Pyramid

--- unit-04/console-pyramid/main.js
+++ unit-04/console-pyramid/main.js
@@ -1,44 +1,35 @@
 function drawConsolePyramid(height) {
   const pyramid = createPyramidArray(height);
 
-  pyramid.forEach((row) => {
-    // console.log(row);
-    row.forEach((element) => {
-      // console.log(element);
-      for (let i = 0; i < element; i++) {
-        console.log(printStars(element) + "\n");
-      }
-    });
-  });
-}
-
-function printStars(number) {
-  let numStars = "*";
-
-  for (let i = 1; i < number; i++) {
-    numStars += "**";
-  }
-
-  return numStars;
+  // pyramid.forEach((row) => {
+  //   // console.log(row);
+  //   row.forEach((element) => {
+  //     // console.log(element);
+  //     for (let i = 0; i < element; i++) {
+  //       console.log(printStars(element) + "\n");
+  //     }
+  //   });
+  // });
 }
 
 function createPyramidArray(height) {
-  const arrayPyramid = [];
-  let num = 1;
-  let spacer = height - 1;
+  let num = "*";
+  let spacerCount = height - 1;
 
   for (let i = 0; i < height; i++) {
-    arrayPyramid.push([spacer, num]);
-    num += 2;
-    spacer--;
+    let spacer = "";
+    for (let j = 0; j < spacerCount; j++) {
+      spacer += ` `;
+    }
+    console.log(`${spacer}${num}`);
+    num += "**";
+    spacerCount--;
   }
-
-  return arrayPyramid;
 }
 
 drawConsolePyramid(2);
-// drawConsolePyramid(4);
-// drawConsolePyramid(5);
-// drawConsolePyramid(7);
-// drawConsolePyramid(8);
-// drawConsolePyramid(2);
+drawConsolePyramid(4);
+drawConsolePyramid(5);
+drawConsolePyramid(7);
+drawConsolePyramid(8);
+drawConsolePyramid(2);

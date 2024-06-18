40a5083280144e6fd0a867ab9058fba0fadb2a74
Paddy <paddyfedoo@hotmail.com>
2024-06-18 22:02:35 +0100

    Testing YouTube Embed

---
+++ TestingMovieDB/testing-youtube-embed.js
@@ -0,0 +1,466 @@
+const godFatherVideos = {
+  id: 238,
+  results: [
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Al Pacino’s Audition Tape for The Godfather",
+      key: "E7CMIhJiTXY",
+      site: "YouTube",
+      size: 720,
+      type: "Featurette",
+      official: true,
+      published_at: "2024-06-05T02:48:59.000Z",
+      id: "665fe51ea14cf45250589fe0",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: 'Tom Hanks on Brando\'s Iconic "Godfather" Look',
+      key: "GiZVLFvQruQ",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: false,
+      published_at: "2024-01-01T15:05:00.000Z",
+      id: "65c4b99b194186017b7600d1",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Don Corleone Gets Shot",
+      key: "evl5ai7Mg7w",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: true,
+      published_at: "2023-12-02T16:59:55.000Z",
+      id: "656b9c210859b400ff7519bc",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "The Art of Moviemaking: The Godfather | Academy Conversations",
+      key: "1cdrtPIzjII",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2023-03-21T01:06:03.000Z",
+      id: "65a2c798e9c0dc0126a4367b",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "THE GODFATHER 50th Anniversary Franchise Trailer",
+      key: "7pfqivkYUlE",
+      site: "YouTube",
+      size: 2160,
+      type: "Trailer",
+      official: false,
+      published_at: "2022-09-22T13:06:42.000Z",
+      id: "665be9001334be13f8480fc5",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: '"Don Vito and Michael Corleone" Clip',
+      key: "7IXVycNgGN0",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: true,
+      published_at: "2022-03-27T15:59:56.000Z",
+      id: "65a2c62c85867801255ba3fe",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Opening Scene",
+      key: "eZHsmb4ezEk",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: true,
+      published_at: "2022-03-22T15:59:49.000Z",
+      id: "627e772c006eee3428a4ae9f",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Mario Puzo on the baptism scene in THE GODFATHER",
+      key: "CG5kZcWj1mE",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2022-03-18T14:20:03.000Z",
+      id: "65a2c6db39549a012610719f",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "50th Anniversary Trailer",
+      key: "Ew9ngL1GZvs",
+      site: "YouTube",
+      size: 2160,
+      type: "Trailer",
+      official: true,
+      published_at: "2022-01-13T13:59:54.000Z",
+      id: "61e4e0704f58010019f1fc53",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Ray Romano announces THE GODFATHER for AFI Movie Club",
+      key: "JxiteCBugh8",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2020-08-21T16:11:40.000Z",
+      id: "65a2c6b11f0275012e0860c6",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "45th Anniversary Spot",
+      key: "gMjmsqvIPGc",
+      site: "YouTube",
+      size: 720,
+      type: "Teaser",
+      official: true,
+      published_at: "2017-05-12T18:30:38.000Z",
+      id: "65a2c7e939549a012310722d",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: '"Sleep With The Fishes" Clip',
+      key: "HsRvgCsLXWw",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: true,
+      published_at: "2017-05-10T17:55:13.000Z",
+      id: "65a2c838a6a4c1012ba476ad",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Offer He Can't Refuse",
+      key: "fBNpSRtfIUA",
+      published_at: "2016-03-06T22:19:42.000Z",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: true,
+      id: "592199669251414ab10568ec",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "The Godfather and The Candidate Win Writing Awards: 1973 Oscars",
+      key: "M3GhqH7QmcA",
+      site: "YouTube",
+      size: 480,
+      type: "Featurette",
+      official: true,
+      published_at: "2013-09-12T17:14:52.000Z",
+      id: "65c4bbae0c2710017e77f157",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Sydney Pollack on THE GODFATHER",
+      key: "-lBTgBqGA-g",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2013-03-23T00:31:08.000Z",
+      id: "65a2c6aad35dea013142c186",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "The Godfather Wins Best Picture: 45th Oscars (1973)",
+      key: "Y1qWRdil--A",
+      site: "YouTube",
+      size: 480,
+      type: "Featurette",
+      official: true,
+      published_at: "2012-08-31T17:33:28.000Z",
+      id: "65c4bb430c2710017e77f0b0",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Original Trailer",
+      key: "8V2k2YQEQJ4",
+      site: "YouTube",
+      size: 1080,
+      type: "Trailer",
+      official: true,
+      published_at: "2012-03-27T08:39:55.000Z",
+      id: "65a2c57885867801255ba3f0",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "William Friedkin on THE GODFATHER",
+      key: "F1L3Tz0Im_8",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2010-09-23T16:44:52.000Z",
+      id: "65a2c69fb7b69d01313c0642",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Sidney Lumet On THE GODFATHER",
+      key: "0Hl_mdNgjpY",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2009-09-30T18:55:30.000Z",
+      id: "65a2c696bc2cb300b901e2b2",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Francis Ford Coppola On Casting Al Pacino in THE GODFATHER",
+      key: "67Fg3xmvJyA",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2009-03-24T00:19:03.000Z",
+      id: "65a2c6ba39549a012c1071ae",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: 'Marlon Brando\'s Best Actor Oscar win for "The Godfather" | Sacheen Littlefeather',
+      key: "2QUacU0I4yU",
+      site: "YouTube",
+      size: 480,
+      type: "Featurette",
+      official: true,
+      published_at: "2008-10-03T00:49:40.000Z",
+      id: "65c4bb79fd6fa101877961a0",
+    },
+  ],
+};
+const starWarsVideos = {
+  id: 11,
+  results: [
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars (1977) - Trailer",
+      key: "XsS1yE2f-hE",
+      site: "YouTube",
+      size: 1080,
+      type: "Trailer",
+      official: false,
+      published_at: "2022-05-25T16:00:29.000Z",
+      id: "63d1c26ecb71b800810237b7",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars (1977) - Teaser Trailer",
+      key: "jVyXow0Qki0",
+      site: "YouTube",
+      size: 1080,
+      type: "Teaser",
+      official: false,
+      published_at: "2022-05-25T13:00:31.000Z",
+      id: "63d1c2e6e72fe800aa01d430",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars: A New Hope | All the Facts | Disney+ Deets",
+      key: "SCBJJG_Ncl0",
+      site: "YouTube",
+      size: 1080,
+      type: "Featurette",
+      official: true,
+      published_at: "2021-05-03T16:00:12.000Z",
+      id: "62c041a322e4800fa8f138eb",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars - briefing scene HD",
+      key: "TOgtj00Rp8s",
+      site: "YouTube",
+      size: 1080,
+      type: "Clip",
+      official: false,
+      published_at: "2016-12-16T02:03:35.000Z",
+      id: "64b073dc8a0e9b013aff641e",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars: A New Hope l Ben Kenobi Appears",
+      key: "oTV2tS4nRPE",
+      site: "YouTube",
+      size: 720,
+      type: "Clip",
+      official: false,
+      published_at: "2016-09-17T12:52:09.000Z",
+      id: "64b072c03e2ec8012ee6db8c",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "TIE Fighter Attack A New Hope",
+      key: "mSvPxNopdHs",
+      site: "YouTube",
+      size: 720,
+      type: "Clip",
+      official: false,
+      published_at: "2016-06-27T06:54:31.000Z",
+      id: "64b074108a0e9b011d8e8d31",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Star Wars: A New Hope Bloopers",
+      key: "CNfIQVplY9U",
+      site: "YouTube",
+      size: 360,
+      type: "Bloopers",
+      official: false,
+      published_at: "2014-10-15T16:57:16.000Z",
+      id: "654189f413654500fc8a479f",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Teaser Trailer",
+      key: "i-vsILeJ8_8",
+      published_at: "2013-10-09T19:00:04.000Z",
+      site: "YouTube",
+      size: 1080,
+      type: "Teaser",
+      official: true,
+      id: "533ec651c3a368544800000b",
+    },
+    {
+      iso_639_1: "en",
+      iso_3166_1: "US",
+      name: "Trailer",
+      key: "vZ734NWnAHA",
+      published_at: "2012-07-05T23:24:48.000Z",
+      site: "YouTube",
+      size: 1080,
+      type: "Teaser",
+      official: true,
+      id: "533ec651c3a368544800000a",
+    },
+  ],
+};
+
+const movieForm = document.querySelector("#movieForm");
+const submitButton = movieForm.querySelector("#submit");
+
+function getData() {
+  let thisIsTheFormData = new FormData(movieForm, submitButton);
+  for (const [key, value] of thisIsTheFormData) {
+    if (key === "movieId")
+      getJsonAPI(value).catch((error) => console.error(error));
+  }
+}
+
+window.addEventListener("load", (event) => {
+  movieForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getData();
+  });
+});
+
+function displayVideos(data) {
+  console.log(data);
+  let outputHtml = "";
+
+  outputHtml += "<ul>";
+
+  data.results
+    .filter((element) => {
+      return element.official === true && element.type === "Trailer";
+    })
+    .forEach((element) => {
+      outputHtml += `<li>${element.name}<iframe
+      id="ytplayer"
+      type="text/html"
+      width="480"
+      height="270"
+      src="https://www.youtube.com/embed/${element.key}"
+      frameborder="0"
+      allowfullscreen
+    ></iframe></li>`;
+    });
+
+  outputHtml += `</ul>`;
+
+  return outputHtml;
+}
+
+const videoList = document.querySelector("#videoList");
+
+// videoList.innerHTML = displayVideos(godFatherVideos);
+
+const apiResult = document.querySelector("#apiResult");
+
+// const promise = getMovieInfo();
+// const results = promise
+//   .then((data) => {
+//     console.log("inner", data.results);
+//     return data.results;
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+// console.log("results", results);
+
+// apiResult.innerHTML = displayVideos(results);
+
+async function getJsonAPI(movieId) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        // ENTER AUTHORIZATION CODE HERE
+        "enter authorization code here",
+    },
+  };
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+    options
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTPerror:${response.status}`);
+  }
+
+  const data = await response.json();
+  videoList.innerHTML = displayVideos(data);
+}
+
+// getJsonAPI();
+
+function getMovieInfo() {
+  const data = getJsonAPI();
+  return data;
+}

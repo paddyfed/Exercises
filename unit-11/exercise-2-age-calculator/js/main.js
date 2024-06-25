beb0070d9f2736ce220e9b4abfc7c708408be2ad
Paddy <paddyfedoo@hotmail.com>
2024-06-25 13:08:43 +0100

    Calculating max attribute and value attribute

--- unit-11/exercise-2-age-calculator/js/main.js
+++ unit-11/exercise-2-age-calculator/js/main.js
@@ -2,6 +2,12 @@
   const ageCalculatorForm = document.querySelector("#ageCalculatorForm");
   const submitButton = ageCalculatorForm.querySelector("#submit");
   const resultMessage = document.querySelector("#ageResultMessage");
+  const ageInput = ageCalculatorForm.querySelector("#ageInput");
+
+  const today = new Date();
+
+  ageInput.setAttribute("max", today.toISOString().split("T")[0]);
+  ageInput.setAttribute("value", today.toISOString().split("T")[0]);
 
   function getData() {
     let thisIsTheFormData = new FormData(ageCalculatorForm, submitButton);
